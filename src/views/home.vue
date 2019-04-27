3485c45f9c9ea0a4397cf00abc79fbfecc712d92
stenry <chentianhengg@gmail.com>
2019-04-27 17:12:41 +0800

    初次版本更新

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,13 @@
 <template>
-    $END$
+<div id="container" >
+    <div id="banner" class="bannerImg">
+        <h1>微信生态+PC网页端</h1>
+        <h1>一体化酒店智慧营销管理解决方案</h1>
+        <h2>助力粉丝留存，增加酒店收入</h2>
+        <a href="#">免费试用30天</a>
+    </div>
+    <header></header>
+</div>
 </template>
 
 <script>
@@ -9,5 +17,32 @@
 </script>
 
 <style scoped>
+        /*容器的样式*/
+        #container{
+            display: flex;
+        }
+
+        /*banner的样式*/
+        .bannerImg {
+            background-image: url("../assets/banner.png");
+            background-repeat: no-repeat;
+            background-size: 100%;
+            width: 100%;
+            height: 600px;
+            min-width: 1275px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .bannerImg h1{
+
+        }
+
+        .bannerImg a{
+            
+        }
+
 
 </style>
